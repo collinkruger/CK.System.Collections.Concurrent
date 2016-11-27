3c1a9b34aa7a19673585425de20b2c25570f8935
Collin Kruger <collin.kruger@live.com>
2016-11-27 08:41:03 -0600

    Initial

---
+++ CK.System.Collection.Concurrent/ConcurrentHashSet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK.System.Collection.Concurrent
+{
+    public class ConcurrentHashSet<T> : IEnumerable<T>
+    {
+        // Fields -------------------------------------------
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        readonly ConcurrentDictionary<T, object> dic;
+
+
+        // Ctor ---------------------------------------------
+
+        public ConcurrentHashSet()
+        {
+            dic = new ConcurrentDictionary<T, object>();
+        }
+
+        public ConcurrentHashSet(IEnumerable<T> initialItems)
+        {
+            if (initialItems == null)
+                throw new ArgumentNullException(nameof(initialItems));
+
+            dic = new ConcurrentDictionary<T, object>(initialItems.Select(x => new KeyValuePair<T, object>(x, null)));
+        }
+
+
+        // Read Through Props -------------------------------
+
+        public int Count => dic.Count;
+
+
+        // Methods ------------------------------------------
+
+        public bool TryAdd(T obj) => dic.TryAdd(obj, null);
+
+        public bool TryRemove(T obj)
+        {
+            object _;
+            return dic.TryRemove(obj, out _);
+        }
+
+        public bool Contains(T obj) => dic.ContainsKey(obj);
+
+
+        // IEnumerable<T> -----------------------------------
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var kvp in dic)
+                yield return kvp.Key;
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ CK.System.Collection.Concurrent/ConcurrentHashSetExt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK.System.Collection.Concurrent
+{
+    public static class ConcurrentHashSetExt
+    {
+        public static ConcurrentHashSet<T> ToConcurrentHashSet<T>(this IEnumerable<T> items) => new ConcurrentHashSet<T>(items);
+    }
+}
